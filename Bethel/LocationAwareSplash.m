4dc5e47d9770ad6ffcd7ec7ed3809d92eeb95814
Albert Martin <albertmartin@mac.com>
2014-04-08 23:31:11 -0400

    Added placeholder location splash screen.

---
+++ Bethel/LocationAwareSplash.m
@@ -0,0 +1,23 @@
+//
+//  LocationAwareSplash.m
+//  Bethel
+//
+//  Created by Albert Martin on 4/8/14.
+//  Copyright (c) 2014 Albert Martin. All rights reserved.
+//
+
+#import "LocationAwareSplash.h"
+
+@implementation LocationAwareSplash
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+@end
