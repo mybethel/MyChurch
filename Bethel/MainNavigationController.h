21553bbd287fafbf1f12452d255ef6f16e9673b7
Albert Martin <albertmartin@mac.com>
2014-04-08 20:01:12 -0400

    Disable landscape autorotation.

---
+++ Bethel/MainNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MainNavigationController.h
+//  Bethel
+//
+//  Created by Albert Martin on 4/8/14.
+//  Copyright (c) 2014 Albert Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainNavigationController : UINavigationController
+
+@end
